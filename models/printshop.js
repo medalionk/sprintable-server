9b5b1fb19b36532de7e560e7ea7807cc68a457a9
medalionk <abdullahbilal88@gmail.com>
2017-08-03 19:18:12 +0300

    Added Printshop model

--- models/printshop.js
+++ models/printshop.js
@@ -1,35 +1,20 @@
-var Status = {
-    OPEN: "OPEN",
-    ACCEPTED: "ACCEPTED",
-    REJECTED: "REJECTED",
-    PROCESSING: "PROCESSING",
-    CLOSED: "CLOSED"
-};
-
-var Job = function(title, description, created_date, delivery_date, price, quantity,
-                   delivery_address, image, status, customer_id) {
-    this.title = title;
-    this.description = description;
-    this.created_date = created_date;
-    this.delivery_date = delivery_date;
-    this.price = price;
-    this.quantity = quantity;
-    this.delivery_address = delivery_address;
-    this.image = image;
-    this.status = status;
-    this.customer_id = customer_id;
+var Printshop = function(name, note, address, thumbnail, rating, reg_date) {
+    this.name = name;
+    this.note = note;
+    this.address = address;
+    this.thumbnail = thumbnail;
+    this.rating = rating;
+    this.reg_date = reg_date;
 };
 
 exports.create = function(req, next) {
     var body = req.body;
-    if (!body || !body.title || !body.description || !body.delivery_date || !body.price
-        || !body.quantity || !body.delivery_address || !body.image || !body.customer_id)
+    if (!body || !body.name || !body.address || !body.thumbnail)
         return next(new Error('Invalid data provided.'));
 
-    var job = new Job(body.title, body.description, new Date(), body.delivery_date, body.price,
-        body.quantity, body.delivery_address, body.image, Status.OPEN, body.customer_id);
-    return job;
+    var printshop = new Printshop(body.name, body.note || '', body.address,
+        body.thumbnail, 0.0, new Date());
+    return printshop;
 };
 
-exports.Job = Job;
-exports.Status = Status;
+exports.Printshop = Printshop;
